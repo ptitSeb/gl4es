35438fb4c64e16ca7859c4f76bbd8ea197038277
Ryan Hileman <lunixbochs@gmail.com>
2012-12-13 03:24:16 -0800

    add glx stubs

--- src/glesglx.c
+++ src/glesglx.c
@@ -4,10 +4,20 @@ GLXContext glXCreateContext(Display *display,
                             XVisualInfo *visual,
                             GLXContext shareList,
                             Bool isDirect) {
-    // make an egl context here
-    return NULL;
+    // make an egl context here... need to return a glx context kinda pointing at it
+    // EGLContext egl = eglCreateContext(display, config, shareList)
+    return;
 }
 
 void glXDestroyContext(Display *display, GLXContext ctx) {
     return;
 }
+
+void glXGetProcAddressARB() {}
+void glXChooseVisual() {}
+void glXMakeCurrent() {}
+void glXSwapBuffers() {}
+void glXGetConfig() {}
+void glXQueryExtensionsString() {}
+void glXSwapIntervalSGI() {}
+void glXSwapIntervalMESA() {}
