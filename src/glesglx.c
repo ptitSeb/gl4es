e74ccff474f29140557e71f35df1f13ad03bedb8
Ryan Hileman <lunixbochs@gmail.com>
2012-12-14 03:29:22 -0800

    glXCreateContext/glXDestroyContext working

--- src/glesglx.c
+++ src/glesglx.c
@@ -1,20 +1,77 @@
 #include <glesglx.h>
-#include <stdio.h>
 
 #define maxConfigs 20
+
+bool eglInitialized = false;
+EGLDisplay eglDisplay;
+EGLSurface eglSurface;
+EGLConfig eglConfig;
 
 GLXContext glXCreateContext(Display *display,
                             XVisualInfo *visual,
                             GLXContext shareList,
                             Bool isDirect) {
     printf("glXCreateContext\n");
-    // make an egl context here... need to return a glx context kinda pointing at it
-    // EGLContext egl = eglCreateContext(display, config, shareList)
-    return;
+    GLXContext fake = {0, 0, NULL, 0, 0};
+    display = XOpenDisplay(NULL);
+
+    // make an egl context here...
+    EGLBoolean result;
+    if (!eglDisplay) {
+        eglDisplay = eglGetDisplay(display);
+        if (eglDisplay == EGL_NO_DISPLAY) {
+            printf("Unable to create EGL display.\n");
+            return fake;
+        }
+    }
+
+    // first time?
+    if (eglInitialized == false) {
+        eglInitialized = true;
+        result = eglInitialize(eglDisplay, NULL, NULL);
+        if (result != EGL_TRUE) {
+            printf("Unable to initialize EGL display.\n");
+            return fake;
+        }
+    }
+
+    EGLint eglAttrs[] = {
+        EGL_RED_SIZE, 5,
+        EGL_GREEN_SIZE, 6,
+        EGL_BLUE_SIZE, 5,
+        EGL_DEPTH_SIZE, 16,
+        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_ES_BIT,
+        EGL_NONE,
+    };
+
+    int configsFound;
+    EGLConfig *configs = malloc(sizeof(EGLConfig) * maxConfigs);
+    result = eglChooseConfig(eglDisplay, eglAttrs, configs, maxConfigs, &configsFound);
+    if (result != EGL_TRUE || configsFound == 0) {
+        printf("No EGL configs found.\n");
+        return fake;
+    }
+    eglConfig = configs[0];
+    EGLContext egl = eglCreateContext(display, eglConfig, NULL, eglAttrs);
+
+    // need to return a glx context pointing at it
+    return (GLXContext){display, true, &egl, 0, 0};;
 }
 
 void glXDestroyContext(Display *display, GLXContext ctx) {
     printf("glXDestroyContext\n");
+    display = XOpenDisplay(NULL);
+    if (ctx.egl != NULL) {
+        EGLBoolean result = eglDestroyContext(display, *ctx.egl);
+        if (eglSurface != NULL) {
+            eglDestroySurface(eglDisplay, eglSurface);
+        }
+
+        if (result != EGL_TRUE) {
+            printf("Failed to destroy EGL context.\n");
+        }
+    }
     return;
 }
 
@@ -27,47 +84,33 @@ XVisualInfo *glXChooseVisual(Display *display,
                              int screen,
                              int *attributes) {
     printf("glXChooseVisual\n");
-    int configsFound = 0;
-    EGLBoolean result;
-
-    EGLConfig *configs = malloc(sizeof(EGLConfig) * maxConfigs);
-    result = eglChooseConfig(display, attributes, configs, maxConfigs, &configsFound);
-    if (result != EGL_TRUE || configsFound == 0) {
-        printf("No EGL configs found.\n");
-        return NULL;
-    }
-    printf("%i configs found\n", configsFound);
-
-    XVisualInfo *visuals = (XVisualInfo *)malloc(sizeof(XVisualInfo) * configsFound);
-    for (int i = 0; i < configsFound; i++) {
-        printf("config %i\n", i);
-        XVisualInfo *visual = (visuals+i);
-        EGLConfig *config = (configs+i);
-
-        // maybe bad if this is ever called more than once and cached
-        visual->visualid = i;
-        visual->screen = screen;
-
-        // http://tronche.com/gui/x/xlib/window/visual-types.html
-        // hope this is good enough :)
-        eglGetConfigAttrib(display, config, EGL_DEPTH_SIZE, &visual->depth);
-        visual->class = TrueColor;
-    }
-
-    printf("exiting function\n");
-    return visuals;
+    XVisualInfo *visual = (XVisualInfo *)malloc(sizeof(XVisualInfo));
+    // apparently can't trust the Display I'm passed?
+    display = XOpenDisplay(NULL);
+    XMatchVisualInfo(display, screen, 16, TrueColor, visual);
+    return visual;
 }
 
 Bool glXMakeCurrent(Display *display,
-                    int  drawable,
+                    int drawable,
                     GLXContext context) {
     printf("glXMakeCurrent\n");
+    EGLContext *ctx = context.egl;
+    eglSurface = eglCreateWindowSurface(eglDisplay, eglConfig, drawable, NULL);
+    printf("here\n");
+    EGLBoolean result = eglMakeCurrent(eglDisplay, eglSurface, eglSurface, *ctx);
+    if (result) {
+        printf("seems to have worked.\n");
+        return true;
+    }
+    CheckEGLErrors();
     return false;
 }
 
 void glXSwapBuffers(Display *display,
                     int drawable) {
     printf("glXSwapBuffers\n");
+    eglSwapBuffers(eglDisplay, eglSurface);
 }
 
 int glXGetConfig(Display *display,
@@ -85,3 +128,4 @@ const char *glXQueryExtensionsString(Display *display,
 }
 void glXSwapIntervalSGI() {}
 void glXSwapIntervalMESA() {}
+// glXCreatePixmap()?
--- src/glesglx.h
+++ src/glesglx.h
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <stdio.h>
 
 typedef struct {
     Display *currentDpy;
     unsigned char isDirect;
-    int currentDrawable;
+    EGLContext *egl;
     int currentReadable;
     XID xid;
 } GLXContext;
@@ -18,3 +19,47 @@ GLXContext glXCreateContext(Display *dpy,
                             Bool direct);
 
 void glXDestroyContext(Display *display, GLXContext ctx);
+
+#define GLX_BUFFER_SIZE         2
+#define GLX_LEVEL               3
+#define GLX_RGBA                4
+#define GLX_RED_SIZE            8
+#define GLX_GREEN_SIZE          9
+#define GLX_BLUE_SIZE           10
+#define GLX_ALPHA_SIZE          11
+#define GLX_DEPTH_SIZE          12
+#define GLX_STENCIL_SIZE        13
+#define GLX_NONE                0x8000
+
+int8_t CheckEGLErrors() {
+    EGLenum error;
+    char *errortext;
+
+    error = eglGetError();
+
+    if (error != EGL_SUCCESS && error != 0)
+    {
+        switch (error)
+        {
+            case EGL_NOT_INITIALIZED:     errortext = "EGL_NOT_INITIALIZED"; break;
+            case EGL_BAD_ACCESS:          errortext = "EGL_BAD_ACCESS"; break;
+            case EGL_BAD_ALLOC:           errortext = "EGL_BAD_ALLOC"; break;
+            case EGL_BAD_ATTRIBUTE:       errortext = "EGL_BAD_ATTRIBUTE"; break;
+            case EGL_BAD_CONTEXT:         errortext = "EGL_BAD_CONTEXT"; break;
+            case EGL_BAD_CONFIG:          errortext = "EGL_BAD_CONFIG"; break;
+            case EGL_BAD_CURRENT_SURFACE: errortext = "EGL_BAD_CURRENT_SURFACE"; break;
+            case EGL_BAD_DISPLAY:         errortext = "EGL_BAD_DISPLAY"; break;
+            case EGL_BAD_SURFACE:         errortext = "EGL_BAD_SURFACE"; break;
+            case EGL_BAD_MATCH:           errortext = "EGL_BAD_MATCH"; break;
+            case EGL_BAD_PARAMETER:       errortext = "EGL_BAD_PARAMETER"; break;
+            case EGL_BAD_NATIVE_PIXMAP:   errortext = "EGL_BAD_NATIVE_PIXMAP"; break;
+            case EGL_BAD_NATIVE_WINDOW:   errortext = "EGL_BAD_NATIVE_WINDOW"; break;
+            default:                      errortext = "unknown"; break;
+        }
+
+        printf("ERROR: EGL Error detected: %s (0x%X)\n", errortext, error);
+        return 1;
+    }
+
+    return 0;
+}
