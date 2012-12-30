4a59f6013870cceee57349cbf1f3e4531a1faa18
Ryan Hileman <lunixbochs@gmail.com>
2012-12-30 02:05:39 -0800

    stub glPush/PopClientAttrib

--- src/gl/glstub.c
+++ src/gl/glstub.c
@@ -8,5 +8,7 @@ def {\
 STUB(void glColorMaterial(GLenum face, GLenum mode));
 STUB(void glPopAttrib());
 STUB(void glPushAttrib(GLbitfield mask));
+STUB(void glPopClientAttrib());
+STUB(void glPushClientAttrib(GLbitfield mask));
 
 #undef STUB
