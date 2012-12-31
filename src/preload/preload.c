8e8933b4217d744bf898f9d16b9e3b65b7a51792
Ryan Hileman <lunixbochs@gmail.com>
2012-12-31 05:24:47 -0800

    improve preload methods

--- src/preload/preload.c
+++ src/preload/preload.c
@@ -2,12 +2,19 @@
 
 SDL_GrabMode SDL_WM_GrabInput(SDL_GrabMode mode) {
     printf("SDL_WM_GrabInput\n");
+    return mode;
 }
 
 int SDL_ShowCursor(int toggle) {
     printf("SDL_ShowCursor\n");
+    return 0;
 }
 
 void SDL_WarpMouse(Uint16 x, Uint16 y) {
     printf("SDL_WM_WarpMouse\n");
 }
+/*
+int SDL_PollEvent(SDL_Event *event) {
+    return 0;
+}
+*/
