7bed7c1752e2bf2b8dc4a32bfa7a9639735099a7
Ryan Hileman <lunixbochs@gmail.com>
2013-01-06 19:06:08 -0800

    update preload, stub SDL_SetGamma

--- src/preload/preload.c
+++ src/preload/preload.c
@@ -6,15 +6,20 @@ SDL_GrabMode SDL_WM_GrabInput(SDL_GrabMode mode) {
 }
 
 int SDL_ShowCursor(int toggle) {
-    printf("SDL_ShowCursor\n");
-    return 0;
+    printf("SDL_ShowCursor(%i)\n", toggle);
+    return toggle;
 }
 
 void SDL_WarpMouse(Uint16 x, Uint16 y) {
     printf("SDL_WM_WarpMouse\n");
 }
+
 /*
 int SDL_PollEvent(SDL_Event *event) {
     return 0;
 }
 */
+
+int SDL_SetGamma(float r, float g, float b) {
+    return 0;
+}
