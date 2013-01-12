d6ddc7234af2b5d414a6cfb46a1786d1d266f960
Ryan Hileman <lunixbochs@gmail.com>
2013-01-12 15:57:31 -0800

    libpreload now supports wrapping XSync

--- src/preload/preload.c
+++ src/preload/preload.c
@@ -1,5 +1,10 @@
-#include <SDL/SDL.h>
+#define _GNU_SOURCE
 
+#include <SDL/SDL.h>
+#include <dlfcn.h>
+#include <X11/Xlib.h>
+
+/*
 SDL_GrabMode SDL_WM_GrabInput(SDL_GrabMode mode) {
     printf("SDL_WM_GrabInput\n");
     return mode;
@@ -14,7 +19,6 @@ void SDL_WarpMouse(Uint16 x, Uint16 y) {
     printf("SDL_WM_WarpMouse\n");
 }
 
-/*
 int SDL_PollEvent(SDL_Event *event) {
     return 0;
 }
@@ -22,4 +26,16 @@ int SDL_PollEvent(SDL_Event *event) {
 
 int SDL_SetGamma(float r, float g, float b) {
     return 0;
+}
+
+typedef int (*XSYNCPTR)(Display *, Bool);
+
+int XSync(Display *display, Bool discard) {
+    discard = 1;
+
+    static XSYNCPTR real_XSync;
+    if (!real_XSync) {
+        real_XSync = (XSYNCPTR)dlsym(RTLD_NEXT, "XSync");
+    }
+    return real_XSync(display, discard);
 }
