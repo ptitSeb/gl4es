c7beb0a918ada3955efa120ab0a5c3840c521fd2
Ryan Hileman <lunixbochs@gmail.com>
2013-03-26 14:59:12 -0700

    add blending constants

--- src/gl/const.h
+++ src/gl/const.h
@@ -187,3 +187,17 @@
 #define GL_UNPACK_SWAP_BYTES     0x0CF0
 #define GL_ZOOM_X                0x0D16
 #define GL_ZOOM_Y                0x0D17
+
+// blending
+#define GL_BLEND                 0x0BE2
+#define GL_BLEND_SRC             0x0BE1
+#define GL_BLEND_DST             0x0BE0
+#define GL_SRC_COLOR             0x0300
+#define GL_ONE_MINUS_SRC_COLOR   0x0301
+#define GL_SRC_ALPHA             0x0302
+#define GL_ONE_MINUS_SRC_ALPHA   0x0303
+#define GL_DST_ALPHA             0x0304
+#define GL_ONE_MINUS_DST_ALPHA   0x0305
+#define GL_DST_COLOR             0x0306
+#define GL_ONE_MINUS_DST_COLOR   0x0307
+#define GL_SRC_ALPHA_SATURATE    0x0308
