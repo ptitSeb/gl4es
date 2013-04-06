5d0b2d78cfab738bbafd59b3b7b18b62bbdbfbe1
Ryan Hileman <lunixbochs@gmail.com>
2013-04-06 11:28:30 -0700

    move spec defines to config.h

--- src/gl/gl.h
+++ src/gl/gl.h
@@ -10,6 +10,7 @@
 #ifndef GL_H
 #define GL_H
 
+#include "config.h"
 #include "wrap/gles.h"
 #include "const.h"
 
@@ -104,50 +105,6 @@ static const GLsizei gl_sizeof(GLenum type) {
 
 #include "state.h"
 extern GLstate state;
-
-// don't auto-wrap these functions
-#define skip_glColor4f
-#define skip_glColor4ub
-#define skip_glDisable
-#define skip_glEnable
-#define skip_glGetString
-#define skip_glMaterialfv
-#define skip_glPixelStorei
-#define skip_glTexImage2D
-#define skip_glViewport
-
-// glDrawArrays
-#define skip_glDrawArrays
-#define skip_glVertexPointer
-#define skip_glColorPointer
-#define skip_glNormalPointer
-#define skip_glTexCoordPointer
-#define skip_glDisableClientState
-#define skip_glEnableClientState
-
-// don't compile these into display lists
-#define direct_glColorPointer
-#define direct_glDeleteLists
-#define direct_glDisableClientState
-#define direct_glEdgeFlagPointer
-#define direct_glEnableClientState
-#define direct_glFeedbackBuffer
-#define direct_glFinish
-#define direct_glFlush
-#define direct_glGenLists
-#define direct_glIndexPointer
-#define direct_glInterleavedArrays
-#define direct_glIsEnabled
-#define direct_glIsList
-#define direct_glNormalPointer
-#define direct_glPopClientAttrib
-#define direct_glPixelStore
-#define direct_glPushClientAttrib
-#define direct_glReadPixels
-#define direct_glRenderMode
-#define direct_glSelectBuffer
-#define direct_glTexCoordPointer
-#define direct_glVertexPointer
 
 // newly-defined GL functions
 GLboolean glIsList(GLuint list);
