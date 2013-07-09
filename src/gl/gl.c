3f87e1cbccafb83ed1bf7afc57846883acf12ffc
Ryan Hileman <lunixbochs@gmail.com>
2013-07-09 09:46:55 -0700

    add some GL_EXTENSIONS for ES2 mode

--- src/gl/gl.c
+++ src/gl/gl.c
@@ -12,6 +12,15 @@ const GLubyte *glGetString(GLenum name) {
 #else
             return (GLubyte *)"1.4 glshim wrapper";
 #endif
+#ifdef USE_ES2
+        case GL_EXTENSIONS:
+            return (const GLubyte *)(char *){
+                "GL_ARB_vertex_shader "
+                "GL_ARB_fragment_shader "
+                "GL_ARB_vertex_buffer_object "
+                "GL_EXT_framebuffer_object "
+            };
+#endif
         default:
             return gles_glGetString(name);
     }
