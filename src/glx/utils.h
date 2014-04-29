36e57c1743147b33b776fa89d56bec8346106cef
ptitSeb <sebastien.chev@gmail.com>
2014-04-29 14:57:22 +0200

    some refactoring in glx

---
+++ src/glx/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../gl/gl.h"
+
+static void
+fill_bitmap(Display * dpy, Window win, GC gc,
+            unsigned int width, unsigned int height,
+            int x0, int y0, unsigned int c, GLubyte * bitmap);
+
+static XCharStruct *
+isvalid(XFontStruct * fs, int which);
+
+#endif
