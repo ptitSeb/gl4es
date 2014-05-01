435bd5ce4abff38f69944994091825e9d60b717e
ptitSeb <sebastien.chev@gmail.com>
2014-05-01 22:34:16 +0200

    Improved the downscaling

--- src/gl/pixel.c
+++ src/gl/pixel.c
@@ -201,6 +201,113 @@ bool transform_pixel(const GLvoid *src, GLvoid *dst,
     #undef write_each
 }
 
+static inline
+bool half_pixel(const GLvoid *src0, const GLvoid *src1, 
+                 const GLvoid *src2, const GLvoid *src3, 
+                 GLvoid *dst, 
+                 const colorlayout_t *src_color, GLenum src_type) {
+
+    #define type_case(constant, type, ...)        \
+        case constant: {                          \
+            const type *s[4];                     \
+            s[0] = (const type *)src0;            \
+            s[1] = (const type *)src1;            \
+            s[2] = (const type *)src2;            \
+            s[3] = (const type *)src3;            \
+            type *d = (type *)dst;                \
+            type v[4];                            \
+            v[0] = *s[0];                         \
+            v[1] = *s[1];                         \
+            v[2] = *s[2];                         \
+            v[3] = *s[3];                         \
+            __VA_ARGS__                           \
+            break;                                \
+        }
+
+    #define default(arr, amod, vmod, key, def) \
+        key >= 0 ? arr[amod key] vmod : def
+
+    #define carefully(arr, amod, key, value) \
+        if (key >= 0) d[amod key] = value;
+
+    #define read_i_each(amod, vmod, i)                                   \
+        pix[i].r = default(s[i], amod, vmod, src_color->red, 0.0f);      \
+        pix[i].g = default(s[i], amod, vmod, src_color->green, 0.0f);    \
+        pix[i].b = default(s[i], amod, vmod, src_color->blue, 0.0f);     \
+        pix[i].a = default(s[i], amod, vmod, src_color->alpha, 1.0f);
+
+    #define read_each(amod, vmod)   \
+        read_i_each(amod, vmod, 0);   \
+        read_i_each(amod, vmod, 1);   \
+        read_i_each(amod, vmod, 2);   \
+        read_i_each(amod, vmod, 3);
+
+    #define write_each(amod, vmod)                         \
+        carefully(d, amod, src_color->red, pixel.r vmod)   \
+        carefully(d, amod, src_color->green, pixel.g vmod) \
+        carefully(d, amod, src_color->blue, pixel.b vmod)  \
+        carefully(d, amod, src_color->alpha, pixel.a vmod)
+
+    // this pixel stores our intermediate color
+    // it will be RGBA and normalized to between (0.0 - 1.0f)
+    pixel_t pix[4], pixel;
+    switch (src_type) {
+        type_case(GL_DOUBLE, GLdouble, read_each(,))
+        type_case(GL_FLOAT, GLfloat, read_each(,))
+        case GL_UNSIGNED_INT_8_8_8_8_REV:
+        type_case(GL_UNSIGNED_BYTE, GLubyte, read_each(, / 255.0f))
+        type_case(GL_UNSIGNED_INT_8_8_8_8, GLubyte, read_each(3 - , / 255.0f))
+        type_case(GL_UNSIGNED_SHORT_1_5_5_5_REV, GLushort,
+            for (int ii=0; ii<4; ii++) {
+                s[ii] = (GLushort[]) {
+                    (v[ii] & 31),
+                    ((v[ii] & 0x03e0) >> 5),
+                    ((v[ii] & 0x7c00) >> 10),
+                    ((v[ii] & 0x8000) >> 15)*31,
+                };
+            };
+            read_each(, / 31.0f);
+        )
+        default:
+            // TODO: add glSetError?
+            printf("libGL: Unsupported source data type: %i\n", src_type);
+            return false;
+            break;
+    }
+    pixel.r = (pix[0].r + pix[1].r + pix[2].r + pix[3].r) * 0.25f;
+    pixel.g = (pix[0].g + pix[1].g + pix[2].g + pix[3].g) * 0.25f;
+    pixel.b = (pix[0].b + pix[1].b + pix[2].b + pix[3].b) * 0.25f;
+    pixel.a = (pix[0].a + pix[1].a + pix[2].a + pix[3].a) * 0.25f;
+
+    switch (src_type) {
+        type_case(GL_FLOAT, GLfloat, write_each(,))
+        type_case(GL_UNSIGNED_BYTE, GLubyte, write_each(, * 255.0))
+        // TODO: force 565 to RGB? then we can change [4] -> 3
+        type_case(GL_UNSIGNED_SHORT_5_6_5, GLushort,
+            GLfloat color[4];
+            color[src_color->red] = pixel.r;
+            color[src_color->green] = pixel.g;
+            color[src_color->blue] = pixel.b;
+            *d = ((GLuint)(color[0] * 31) & 0x1f << 11) |
+                 ((GLuint)(color[1] * 63) & 0x3f << 5) |
+                 ((GLuint)(color[2] * 31) & 0x1f);
+        )
+        default:
+            printf("libGL: Unsupported target data type: %i\n", src_type);
+            return false;
+            break;
+    }
+    return true;
+
+    #undef type_case
+    #undef default
+    #undef carefully
+    #undef read_each
+    #undef read_each_i
+    #undef write_each
+}
+
+
 bool pixel_convert(const GLvoid *src, GLvoid **dst,
                    GLuint width, GLuint height,
                    GLenum src_format, GLenum src_type,
@@ -312,8 +419,10 @@ bool pixel_halfscale(const GLvoid *old, GLvoid **new,
         return false;
     }
     printf("LIBGL: halfscaling %ux%u -> %ux%u\n", width, height, new_width, new_height);
+    const colorlayout_t *src_color;
+    src_color = get_color_map(format);
     GLvoid *dst;
-    uintptr_t src, pos, pixel;
+    uintptr_t src, pos, pix0, pix1, pix2, pix3;
 
     pixel_size = pixel_sizeof(format, type);
     dst = malloc(pixel_size * new_width * new_height);
@@ -321,9 +430,15 @@ bool pixel_halfscale(const GLvoid *old, GLvoid **new,
     pos = (uintptr_t)dst;
     for (int y = 0; y < new_height; y++) {
         for (int x = 0; x < new_width; x++) {
-            pixel = src + ((x * 2) +
+            pix0 = src + ((x * 2) +
                           (y * 2) * width) * pixel_size;
-            memcpy((GLvoid *)pos, (GLvoid *)pixel, pixel_size);
+            pix1 = src + ((x * 2 + 1) +
+                          (y * 2) * width) * pixel_size;
+            pix2 = src + ((x * 2) +
+                          (y * 2 + 1) * width) * pixel_size;
+            pix3 = src + ((x * 2 + 1) +
+                          (y * 2 + 1) * width) * pixel_size;
+            half_pixel((GLvoid *)pix0, (GLvoid *)pix1, (GLvoid *)pix2, (GLvoid *)pix3, (GLvoid *)pos, src_color, type);
             pos += pixel_size;
         }
     }
