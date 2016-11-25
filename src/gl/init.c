adf4d88d4aa2981023cb6f6eec419c48b728dd4d
ptitSeb <sebastien.chev@gmail.com>
2016-11-25 03:56:13 +0100

    Added an option in Android.mk to avoid having initialize_gl4es as a constructor (but must be called by the app as soon as possible), to avoid lock on 4.0.x

--- src/gl/init.c
+++ src/gl/init.c
@@ -31,8 +31,16 @@ static void fast_math() {
 void load_libs();
 void glx_init();
 
+#ifdef NO_INIT_CONSTRUCTOR
+__attribute((visibility("default")))
+#else
 __attribute__((constructor))
+#endif
 void initialize_gl4es() {
+    // only init 1 time
+    static int inited = 0;
+    if(inited) return;
+    inited = 1;
     // default init of globals
     memset(&globals4es, 0, sizeof(globals4es));
     globals4es.mergelist = 1;
