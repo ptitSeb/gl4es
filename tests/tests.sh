9647269e6b5928ff6518f2033414dcf67052db86
ptitSeb <sebastien.chev@gmail.com>
2017-06-30 16:44:13 +0200

    Trying so regression testing on Travis

---
+++ tests/tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -z "$1" ];then
+ export LD_LIBRARY_PATH=$1:$LD_LIBRARY_PATH
+fi
+
+export LIBGL_FB=3
+
+TESTS=`dirname "$0"`
+
+pushd "$TESTS" >/dev/null
+
+echo "StuntCarRacer"
+
+if [ -e stuntcarracer.0000180445.png ];then
+    rm stuntcarracer.0000180445.png
+fi
+if [ -e stuntcarracer.trace ];then
+    rm stuntcarracer.trace
+fi
+tar xf ../traces/stuntcarracer.tgz
+apitrace dump-images --calls="180445" stuntcarracer.trace
+result=$(compare -metric AE ../refs/stuntcarracer.0000180445.png stuntcarracer.0000180445.png diff.png 2>&1)
+if [ ! "$result" == "0" ];then
+    popd >/dev/null
+    echo "error, $result pixels diff"
+    exit 1
+fi
+
+# cleanup
+if [ -e stuntcarracer.0000180445.png ];then
+    rm stuntcarracer.0000180445.png
+fi
+if [ -e stuntcarracer.trace ];then
+    rm stuntcarracer.trace
+fi
+if [ -e diff.png ];then
+    rm diff.png
+fi
+
+popd >/dev/null
+echo "All done"
+exit 0
