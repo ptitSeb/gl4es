ptitSeb/gl4es

commit f93a242f49f9f089087c5520e61ed4013454873f

Some fixes(?) and improvement to some state tracking, and some work on FPE (on state traking)

src/gl/gl.c
@@ -93,6 +93,7 @@ void* NewGLState(void* shared_glstate, int es2only) {
     if(hardext.esversion>1) {
         glstate->fpe_state = (fpe_state_t*)malloc(sizeof(fpe_state_t));
         glstate->glsl.es2 = es2only;
+        fpe_Init(glstate);
     }
 
     // init the matrix tracking
@@ -134,6 +135,9 @@ void* NewGLState(void* shared_glstate, int es2only) {
     // Alpha Func
     glstate->alphafunc = GL_ALWAYS;
     glstate->alpharef = 0.0f;
+    // Blend
+    glstate->blendsfactor = GL_ONE;
+    glstate->blenddfactor = GL_ZERO;
     // Point Sprite
     glstate->pointsprite.size = 1.0f;
     glstate->pointsprite.sizeMax = 1.0f;
@@ -161,10 +165,6 @@ void* NewGLState(void* shared_glstate, int es2only) {
         khash_t(programlist) *programs = glstate->glsl.programs = kh_init(programlist);
 		kh_put(programlist, programs, 1, &ret);
 		kh_del(programlist, programs, 1);
-    }
-    if(hardext.esversion!=1) {
-        glstate->fpe = (fpe_fpe_t*)malloc(sizeof(fpe_fpe_t));
-        memset(glstate->fpe, 0, sizeof(fpe_fpe_t));
     }
 
     // Grab ViewPort
@@ -236,8 +236,8 @@ void DeleteGLState(void* oldstate) {
         free(state->blit);
     }
     //TODO: free sharderlist and programlist...
-    if(state->fpe) {
-        free(state->fpe);
+    if(state->fpe_cache) {
+        fpe_Dispose(state);
     }
     // probably missing some things to free here!
 
@@ -303,15 +303,15 @@ static void fpe_changetex(int n, int state)
 
 static void proxy_glEnable(GLenum cap, bool enable, void (*next)(GLenum)) {
     #define proxy_GO(constant, name) \
-        case constant: glstate->enable.name = enable; next(cap); break
+        case constant: if(glstate->enable.name != enable) {glstate->enable.name = enable; next(cap);} break
     #define proxy_GOFPE(constant, name, fct) \
-        case constant: glstate->enable.name = enable; if(glstate->fpe_state) { fct; } else next(cap); break
+        case constant: if(glstate->enable.name != enable) {glstate->enable.name = enable; if(glstate->fpe_state) { fct; } else next(cap);} break
     #define GO(constant, name) \
         case constant: glstate->enable.name = enable; break;
     #define GOFPE(constant, name, fct) \
         case constant: glstate->enable.name = enable; if(glstate->fpe_state) { fct; } break;
     #define proxy_clientGO(constant, name) \
-        case constant: glstate->vao->name = enable; next(cap); break
+        case constant: if (glstate->vao->name != enable) {glstate->vao->name = enable; next(cap);} break
     #define clientGO(constant, name) \
         case constant: glstate->vao->name = enable; break;
 
@@ -333,6 +333,7 @@ static void proxy_glEnable(GLenum cap, bool enable, void (*next)(GLenum)) {
 #endif
     switch (cap) {
         GO(GL_AUTO_NORMAL, auto_normal);
+        proxy_GO(GL_ALPHA_TEST, alpha_test);
         proxy_GO(GL_BLEND, blend);
         case GL_TEXTURE_2D:
             if(enable)
@@ -461,16 +462,28 @@ int Cap2BatchState(GLenum cap) {
         }
         return ENABLED_LAST;
 }
+int Enabled2BatchState(int cap) {
+    switch(cap) {
+        case ENABLED_ALPHA: return 2-glstate->enable.alpha_test;
+        case ENABLED_BLEND: return 2-glstate->enable.blend;
+        case GL_CULL_FACE: return 2-glstate->enable.cull_face;
+        case GL_DEPTH_TEST: return 2-glstate->enable.depth_test;
+        default:
+            return (IS_TEX2D(glstate->enable.texture[cap-ENABLED_TEX2D_TEX0]))?1:2;
+    }
+}
 
 void gl4es_glEnable(GLenum cap) {
     if (glstate->list.active && (glstate->gl_batch && !glstate->list.compiling))  {
         int which_cap = Cap2BatchState(cap);
         if (which_cap<ENABLED_LAST) {
+            if(glstate->statebatch.enabled[which_cap]==0)
+                glstate->statebatch.enabled[which_cap] = Enabled2BatchState(which_cap);
             if ((glstate->statebatch.enabled[which_cap] == 1))
                 return; // nothing to do...
-            if (glstate->statebatch.enabled[which_cap])
-                flush();
-            glstate->statebatch.enabled[which_cap] = 1;
+            flush();
+            if (glstate->list.active)   // change value only if still in list
+                glstate->statebatch.enabled[which_cap] = 1;
         }
     }
 	PUSH_IF_COMPILING(glEnable)
@@ -495,11 +508,13 @@ void gl4es_glDisable(GLenum cap) {
     if (glstate->list.active && (glstate->gl_batch && !glstate->list.compiling))  {
         int which_cap = Cap2BatchState(cap);
         if (which_cap<ENABLED_LAST) {
+            if(glstate->statebatch.enabled[which_cap]==0)
+                glstate->statebatch.enabled[which_cap] = Enabled2BatchState(which_cap);
             if ((glstate->statebatch.enabled[which_cap] == 2))
                 return; // nothing to do...
-            if (glstate->statebatch.enabled[which_cap])
-                flush();
-            glstate->statebatch.enabled[which_cap] = 2;
+            flush();
+            if (glstate->list.active)   // change value only if still in list
+                glstate->statebatch.enabled[which_cap] = 2;
         }
     }
 	PUSH_IF_COMPILING(glDisable)
@@ -557,6 +572,10 @@ GLboolean gl4es_glIsEnabled(GLenum cap) {
     noerrorShim();
     switch (cap) {
         isenabled(GL_AUTO_NORMAL, auto_normal);
+        isenabled(GL_ALPHA_TEST, alpha_test);
+        isenabled(GL_BLEND, blend);
+        isenabled(GL_CULL_FACE, cull_face);
+        isenabled(GL_DEPTH_TEST, depth_test);
         isenabled(GL_LINE_STIPPLE, line_stipple);
         isenabled(GL_TEXTURE_GEN_S, texgen_s[glstate->texture.active]);
         isenabled(GL_TEXTURE_GEN_T, texgen_t[glstate->texture.active]);

src/gl/blend.c
@@ -56,6 +56,10 @@ void glBlendEquationSeparateEXT(GLenum modeRGB, GLenum modeA) AliasExport("gl4es
 void gl4es_glBlendFunc(GLenum sfactor, GLenum dfactor) {
     if (glstate->list.active)
         if (!glstate->list.compiling && glstate->gl_batch) {
+            if(glstate->blendsfactor == 0 && glstate->blenddfactor == 0) {
+                glstate->statebatch.blendfunc_s = glstate->blendsfactor;
+                glstate->statebatch.blendfunc_d = glstate->blenddfactor;
+            }
             if ((glstate->statebatch.blendfunc_s == sfactor) && (glstate->statebatch.blendfunc_d == dfactor))
                 return; // nothing to do...
             if (!glstate->statebatch.blendfunc_s) {
@@ -129,7 +133,10 @@ void gl4es_glBlendFunc(GLenum sfactor, GLenum dfactor) {
         // special case, as seen in Xash3D, but it breaks torus_trooper, so behind a parameter
         sfactor = GL_ONE;
     }
-
+    if(glstate->blendsfactor==sfactor && glstate->blenddfactor==dfactor)
+        return; // already set
+    glstate->blendsfactor = sfactor;
+    glstate->blenddfactor = dfactor;
 #ifdef ODROID
     if(gles_glBlendFunc)
 #endif

src/gl/fpe.c
@@ -18,6 +18,71 @@
 
 // ********* Cache handling *********
 
+fpe_cache_t* fpe_NewCache() {
+    fpe_cache_t *ret = (fpe_cache_t*)malloc(sizeof(fpe_cache_t));
+    khash_t(fpecachelist) *cache = kh_init(fpecachelist);
+    ret->cache = cache;
+    int r;
+    kh_put(fpecachelist, cache, 1, &r);
+    kh_del(fpecachelist, cache, 1);
+    ret->fpe = (fpe_fpe_t*)malloc(sizeof(fpe_fpe_t));
+    memset(ret->fpe, 0, sizeof(fpe_fpe_t));
+    return ret;
+}
+
+void fpe_Init(glstate_t *glstate) {
+    // initialize cache
+    glstate->fpe_cache = fpe_NewCache();
+}
+
+void fpe_disposeCache(fpe_cache_t* cache) {
+    if(!cache) return;
+    if(cache->fpe) {
+        free(cache->fpe);
+        cache->fpe = NULL;
+    }
+    if(cache->cache) {
+        fpe_cache_t *m;
+        kh_foreach_value((khash_t(fpecachelist)*)cache->cache, m,
+            fpe_disposeCache(m); free(m);
+        )
+        kh_destroy(fpecachelist, cache->cache);
+        free(cache->cache);
+        cache->cache = NULL;
+    }
+}
+
+void fpe_Dispose(glstate_t *glstate) {
+    fpe_disposeCache(glstate->fpe_cache);
+    free(glstate->fpe_cache);
+    glstate->fpe_cache = NULL;
+}
+
+fpe_fpe_t *fpe_GetCache() {
+    fpe_cache_t *cur = glstate->fpe_cache;
+    // multi stage hash search    
+    for(int i=0; i<sizeof(fpe_state_t)/4; i++) {
+        uint32_t t;
+        memcpy(&t, ((void*)&glstate->fpe_state)+sizeof(t)*i, sizeof(t));
+        fpe_cache_t *next = NULL;
+        khint_t k_next;
+        {
+            int ret;
+            khash_t(fpecachelist) *curcache = (khash_t(fpecachelist) *)cur->cache;
+            k_next = kh_get(fpecachelist, curcache, t);
+            if (k_next != kh_end(curcache))
+                cur = kh_value(curcache, k_next);
+            else {
+                k_next = kh_put(fpecachelist, curcache, t, &ret);
+                cur = kh_value(curcache, k_next) = fpe_NewCache();
+            }
+        }
+    }
+
+    return cur->fpe;
+}
+
+
 // ********* Shader stuffs handling *********
 const char* dummy_vertex = \
 "varying vec4 Color; \n"
@@ -33,8 +98,12 @@ const char* dummy_frag = \
 "}\n";
 
 void fpe_program() {
-    // dummy program for now...
+    if(glstate->fpe==NULL || memcmp(&glstate->fpe->state, glstate->fpe_state, sizeof(fpe_state_t))) {
+        // get cached fpe (or new one)
+        glstate->fpe = fpe_GetCache();
+    }   
     if(glstate->fpe->glprogram==NULL) {
+        // dummy program for now...
         glstate->fpe->vert = gl4es_glCreateShader(GL_VERTEX_SHADER);
         gl4es_glShaderSource(glstate->fpe->vert, 1, &dummy_vertex, NULL);
         gl4es_glCompileShader(glstate->fpe->vert);
@@ -54,9 +123,11 @@ void fpe_program() {
             if (k_program != kh_end(programs))
                 glstate->fpe->glprogram = kh_value(programs, k_program);
         }
+        // save relevent state
+        memcpy(&glstate->fpe->state, glstate->fpe_state, sizeof(fpe_state_t));
+        // all done
         DBG(printf("creating dummy FPE shader : %d(%p)\n", glstate->fpe->prog, glstate->fpe->glprogram);)
     }
-
 }
 
 // ********* Fixed Pipeling function wrapper *********

src/gl/fpe.h
@@ -22,24 +22,45 @@
 #define FPE_TEX_CUBE 2
 #define FPE_TEX_STRM 3
 
+#define FPE_ALWAYS   0
+#define FPE_NEVER    1
+#define FPE_LESS     2
+#define FPE_EQUAL    3
+#define FPE_LEQUAL   4
+#define FPE_GREATER  5
+#define FPE_NOTEQUAL 6
+#define FPE_GEQUAL   7
+
 typedef struct {
     int plane:6;                //  0: 5  the 6 planes packed
     int light:8;                //  5:13  the 8 lights packed
     int light_2sided:8;         // 14:21  the 8 two sided bits packed (equation is simpler when not 2-sided)
-    int fogmode:4;              // 22:25  fog mode
-    int colorsum:1;             // 26:26  secondary color enabled
-    int texture:32;             // 27:58  8 texture stored on 4 bits
-    int lightning:1;            // 59:59  global lightning enabled
-    int normalize:1;            // 60:60  normalization
-    int rescaling:1;            // 61:61  rescale normal
+    int fogmode:2;              // 22:23  fog mode
+    int colorsum:1;             // 24:24  secondary color enabled
+    int texture:16;             // 25:40  8 texture stored on 2 bits
+    int lightning:1;            // 41:41  global lightning enabled
+    int normalize:1;            // 42:42  normalization
+    int rescaling:1;            // 43:43  rescale normal
+    int alphfunc:3;             // 44:46  alpha functions
 
-    int dummy:3;                // 62:63  to be sure it's int32 aligned
+    int dummy:17;               // 47:63  to be sure it's int32 aligned
 }__attribute__((packed)) fpe_state_t;
 
 typedef struct {
-  GLuint  frag, vert, prog;   // only dummy shader for now
+  GLuint  frag, vert, prog;   // shader info
+  fpe_state_t state;          // state relevent to the current fpe program
   program_t *glprogram;
 } fpe_fpe_t;
+
+
+typedef struct {
+  void      *cache;
+  fpe_fpe_t *fpe;
+} fpe_cache_t;
+
+KHASH_MAP_INIT_INT(fpecachelist, fpe_cache_t *);
+
+fpe_fpe_t *fpe_GetCache();
 
 void fpe_glEnableClientState(GLenum cap);
 void fpe_glDisableClientState(GLenum cap);

src/gl/gl.h
@@ -429,6 +429,9 @@ void init_batch();
 #include "state.h"
 extern glstate_t *glstate;
 
+void fpe_Init(glstate_t *glstate);       // defined in fpe.c
+void fpe_Dispose(glstate_t *glstate);    // defined in fpe.c
+
 static inline void errorGL() {	// next glGetError will be from GL 
 	glstate->shim_error = 0;
 }

src/gl/state.h
@@ -25,8 +25,11 @@ typedef struct {
               normalize,
               normal_rescale,
               lightning,
+              alpha_test,
               blend,
+              cull_face,
               color_sum,
+              depth_test,
               pointsprite,
               texgen_s[MAX_TEX],
               texgen_t[MAX_TEX],
@@ -255,6 +258,9 @@ typedef struct {
     pointer_state_t color;
     pointer_state_t secondary;
     pointer_state_t tex[MAX_TEX];
+
+    char*           shadersource; // scrach buffer for fpe shader construction
+    int             shadersize;
 } fpestatus_t;
 
 typedef struct {
@@ -310,11 +316,14 @@ typedef struct {
     GLenum              shademodel;
     GLenum              alphafunc;
     GLfloat             alpharef;
+    GLenum              blendsfactor;
+    GLenum              blenddfactor;
     GLenum              logicop;
     glsl_t              glsl;
     fpe_state_t         *fpe_state;
     fpe_fpe_t           *fpe;
     fpestatus_t         fpe_client;
+    fpe_cache_t         *fpe_cache;
     gleshard_t          gleshard;
     glesblit_t          *blit;
 } glstate_t;

commit 53457fe93a14aba1c84302e08f72c3792a0ce36e

Fix some bugs (regression introduced with f93a242f49f9f089087c5520e61ed4013454873f

src/gl/gl.c
@@ -348,6 +348,8 @@ static void proxy_glEnable(GLenum cap, bool enable, void (*next)(GLenum)) {
         GO(GL_AUTO_NORMAL, auto_normal);
         proxy_GOFPE(GL_ALPHA_TEST, alpha_test,glstate->fpe_state->alphatest=enable);
         proxy_GO(GL_BLEND, blend);
+        proxy_GO(GL_CULL_FACE, cull_face);
+        proxy_GO(GL_DEPTH_TEST, depth_test);
         case GL_TEXTURE_2D:
             if(enable)
                 glstate->enable.texture[glstate->texture.active] |= (1<<ENABLED_TEX2D);