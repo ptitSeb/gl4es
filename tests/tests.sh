80f4efc2ca501ccbc962b92bb2e5c20f143f5ca6
ptitSeb <sebastien.chev@gmail.com>
2017-10-19 12:11:27 +0200

    Improved Test script

--- tests/tests.sh
+++ tests/tests.sh
@@ -49,11 +49,36 @@ function clean_tests {
     fi
 }
 
+export OK=1
+function launch_test {
+    tar xf ../traces/$1.tgz
+    apitrace dump-images --calls="$2" $1.trace
+    EXTRACT=""
+    if [ ! -z "$4" ];then
+     EXTRACT="-extract $4"
+    fi
+    result=$(compare -metric AE -fuzz 20% $EXTRACT ../refs/$1.$2.png $1.$2.png diff.png 2>&1)
+    if [ ! "$result" -lt "$3" ];then
+        popd >/dev/null
+        echo "error, $result pixels diff"
+        export OK=0
+    fi
+}
+
+function banner {
+    echo
+    echo " ******************* "
+    echo "    $1 "
+    echo " ------------------- "
+}
+
 if [ ! -z "$1" ];then
  export LD_LIBRARY_PATH=$1:$LD_LIBRARY_PATH
 fi
 
 export LIBGL_FB=3
+export LIBGL_SILENTSTUB=1
+export LIBGL_NOBANNER=1
 
 TESTS=`dirname "$0"`
 
@@ -63,94 +88,54 @@ clean_tests
 
 export LIBGL_ES=1
 
-echo "GLES1.1: glxgears"
-
-tar xf ../traces/glxgears.tgz
-apitrace dump-images --calls="8203" glxgears.trace
-result=$(compare -metric AE -fuzz 20% ../refs/glxgears.0000008203.png glxgears.0000008203.png diff.png 2>&1)
-if [ ! "$result" -lt "25" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES1.1: glxgears"
+launch_test glxgears 0000008203 25
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES1.1: StuntCarRacer"
-
-tar xf ../traces/stuntcarracer.tgz
-apitrace dump-images --calls="118817" stuntcarracer.trace
-result=$(compare -metric AE -fuzz 20% -extract 638x478+1+1 ../refs/stuntcarracer.0000118817.png stuntcarracer.0000118817.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES1.1: StuntCarRacer"
+launch_test stuntcarracer 0000118817 20 638x478+1+1
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES1.1: Neverball"
-
-tar xf ../traces/neverball.tgz
-apitrace dump-images --calls="78750" neverball.trace
-result=$(compare -metric AE -fuzz 20% -extract 798x478+1+1 ../refs/neverball.0000078750.png neverball.0000078750.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES1.1: Neverball"
+launch_test neverball 0000078750 20 798x478+1+1
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES1.1: Foobillard Plus"
-
-tar xf ../traces/foobillardplus.tgz
-apitrace dump-images --calls="14748" foobillardplus.trace
-result=$(compare -metric AE -fuzz 20% -extract 798x478+1+1 ../refs/foobillardplus.0000014748.png foobillardplus.0000014748.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES1.1: Foobillard Plus"
+launch_test foobillardplus 0000014748 20 798x478+1+1
+if [ $OK = 0 ];then
     exit 1
 fi
 
 export LIBGL_ES=2
 
-echo "GLES2.0: OpenRA"
-
-tar xf ../traces/openra.tgz
-apitrace dump-images --calls="31249" openra.trace
-result=$(compare -metric AE -fuzz 20% -extract 638x478+1+1 ../refs/openra.0000031249.png openra.0000031249.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES2.0: OpenRA"
+launch_test openra 0000031249 20 638x478+1+1
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES2.0: glsl_lighting"
-
-tar xf ../traces/glsl_lighting.tgz
-apitrace dump-images --calls="505393" glsl_lighting.trace
-result=$(compare -metric AE -fuzz 20% -extract 638x478+1+1 ../refs/glsl_lighting.0000505393.png glsl_lighting.0000505393.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES2.0: glsl_lighting"
+launch_test glsl_lighting 0000505393 20
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES2.0: glxgears"
-
-tar xf ../traces/glxgears.tgz
-apitrace dump-images --calls="8203" glxgears.trace
-result=$(compare -metric AE -fuzz 20% ../refs/glxgears.0000008203.png glxgears.0000008203.png diff.png 2>&1)
+banner "GLES2.0: glxgears"
 # Because FlatShaded mode is not supported, the image is more different.
-if [ ! "$result" -lt "700" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+launch_test glxgears 0000008203 700
+if [ $OK = 0 ];then
     exit 1
 fi
 
-echo "GLES2.0: StuntCarRacer"
-
-tar xf ../traces/stuntcarracer.tgz
-apitrace dump-images --calls="118817" stuntcarracer.trace
-result=$(compare -metric AE -fuzz 20% -extract 638x478+1+1 ../refs/stuntcarracer.0000118817.png stuntcarracer.0000118817.png diff.png 2>&1)
-if [ ! "$result" -lt "20" ];then
-    popd >/dev/null
-    echo "error, $result pixels diff"
+banner "GLES2.0: StuntCarRacer"
+launch_test stuntcarracer 0000118817 20 638x478+1+1
+if [ $OK = 0 ];then
     exit 1
 fi
 
@@ -158,5 +143,6 @@ fi
 clean_tests
 
 popd >/dev/null
+echo " ================= "
 echo "All done"
 exit 0
