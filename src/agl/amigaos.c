6743ae637a8294de8355f1a6f6a6f0e91a860e30
ptitSeb <sebastien.chev@gmail.com>
2018-02-17 13:57:26 +0100

    [AMIGAOS4] Removed aglXXX function from wrapper, it's not needed

--- src/agl/amigaos.c
+++ src/agl/amigaos.c
@@ -767,13 +767,6 @@ void* os4GetProcAddress(const char* name)
     EX(glVertexAttribPointer)
     EX(glViewport)
     //EX(glPolygonMode) //This is a non-standard function, and gl4es will ignore it (and emulate it), even if Amiga OGLES2 driver implement it
-    // Also export AGL functions
-    EX(aglCreateContext)
-    EX(aglCreateContextTags)
-    EX(aglDestroyContext)
-    EX(aglMakeCurrent)
-    EX(aglSwapBuffers)
-    EX(aglSetBitmap)
 
     return NULL;
 }
