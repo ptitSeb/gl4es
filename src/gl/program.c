1a3644967410901cfd3abe5e6445349108831215
ptitSeb <sebastien.chev@gmail.com>
2019-07-15 15:35:25 +0200

    Refactor glLinkProgram, preparing ground for precompiled shaders

--- src/gl/program.c
+++ src/gl/program.c
@@ -499,12 +499,8 @@ GLboolean gl4es_glIsProgram(GLuint program) {
     return GL_FALSE;
 }
 
-void gl4es_glLinkProgram(GLuint program) {
-    DBG(printf("glLinkProgram(%d)\n", program);)
-    FLUSH_BEGINEND;
-    CHECK_PROGRAM(void, program)
-    noerrorShim();
-
+static void clear_program(program_t *glprogram)
+{
     // clear all Attrib location cache
     if(glprogram->attribloc) {
         attribloc_t *m;
@@ -530,6 +526,141 @@ void gl4es_glLinkProgram(GLuint program) {
         )
     }
     glprogram->cache.size = 0;  // reset cache buffer
+}
+
+static void fill_program(program_t *glprogram)
+{
+    LOAD_GLES(glGetError);
+    LOAD_GLES2(glGetProgramiv);
+    LOAD_GLES2(glGetActiveUniform);
+    LOAD_GLES2(glGetUniformLocation);
+    LOAD_GLES2(glGetActiveAttrib);
+    LOAD_GLES2(glGetAttribLocation);
+    int n=0;
+    int maxsize=0;
+    khint_t k;
+    int ret;
+    DBG(GLenum e2;)
+
+    // init bluitin emulation first
+    builtin_Init(glprogram);
+    // Grab all Uniform
+    gles_glGetProgramiv(glprogram->id, GL_ACTIVE_UNIFORMS, &n);
+    gles_glGetProgramiv(glprogram->id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &maxsize);
+    khash_t(uniformlist) *uniforms = glprogram->uniform;
+    uniform_t *gluniform = NULL;
+    int uniform_cache = 0;
+    GLint size = 0;
+    GLenum type = 0;
+    GLchar *name = (char*)malloc(maxsize);
+    int tu_idx = 0;
+    for (int i=0; i<n; i++) {
+        gles_glGetActiveUniform(glprogram->id, i, maxsize, NULL, &size, &type, name);
+        DBG(e2=gles_glGetError();)
+        DBG(if(e2==GL_NO_ERROR))
+        {
+            // remove any ending "[]" that could be present
+            if(name[strlen(name)-1]==']' && strrchr(name, '[')) (*strrchr(name, '['))='\0';
+            GLint id = gles_glGetUniformLocation(glprogram->id, name);
+            if(id!=-1) {
+                for (int j = 0; j<size; j++) {
+                    k = kh_put(uniformlist, uniforms, id, &ret);
+                    gluniform = kh_value(uniforms, k) = malloc(sizeof(uniform_t));
+                    memset(gluniform, 0, sizeof(uniform_t));
+                    if(j) {
+                        gluniform->name = malloc(strlen(name)+1+5);
+                        sprintf(gluniform->name, "%s[%d]", name, j);
+                    } else
+                        gluniform->name = strdup(name);
+                    gluniform->id = id;
+                    gluniform->internal_id = i;
+                    gluniform->size = size-j;
+                    gluniform->type = type;
+                    gluniform->cache_offs = uniform_cache+j*uniformsize(type);
+                    gluniform->cache_size = uniformsize(type)*(size-j);
+                    gluniform->builtin = builtin_CheckUniform(glprogram, name, id, size-j);
+                    // TextureUnit grabbing...
+                    if(type==GL_SAMPLER_CUBE) {
+                        glprogram->texunits[tu_idx].id = id;
+                        glprogram->texunits[tu_idx].type=TU_CUBE;
+                        glprogram->texunits[tu_idx].req_tu = glprogram->texunits[tu_idx].act_tu = 0;
+                        ++tu_idx;
+                    } else if (type==GL_SAMPLER_2D) {
+                        glprogram->texunits[tu_idx].id = id;
+                        glprogram->texunits[tu_idx].type=TU_TEX2D;
+                        glprogram->texunits[tu_idx].req_tu = glprogram->texunits[tu_idx].act_tu = 0;
+                        ++tu_idx;
+                    }
+                    DBG(printf(" uniform #%d : \"%s\"%s type=%s size=%d\n", id, gluniform->name, gluniform->builtin?" (builtin) ":"", PrintEnum(gluniform->type), gluniform->size);)
+                    if(gluniform->size==1) ++glprogram->num_uniform;
+                    id++;
+                }
+                uniform_cache += uniformsize(type)*size;
+            }
+        }
+        DBG(else printf("LIBGL: Warning, getting Uniform #%d info failed with %s\n", i, PrintEnum(e2));)
+    }
+    free(name);
+    // reset uniform cache
+    if(glprogram->cache.cap < uniform_cache) {
+        glprogram->cache.cap=uniform_cache;
+        glprogram->cache.cache = malloc(glprogram->cache.cap);
+    }
+    memset(glprogram->cache.cache, 0, glprogram->cache.cap);
+    //Maybe Sampler uniform should not be initialized to 0, but to -1, to be sure the value is initialized?
+    if(glprogram->uniform) {
+        uniform_t *m;
+        khint_t k;
+        kh_foreach(glprogram->uniform, k, m,
+            if(m->type == GL_SAMPLER_2D || m->type == GL_SAMPLER_CUBE)
+                memset(glprogram->cache.cache+m->cache_offs, 0xff, m->cache_size);
+        )
+    }
+
+    // Grab all Attrib
+    gles_glGetProgramiv(glprogram->id, GL_ACTIVE_ATTRIBUTES, &n);
+    gles_glGetProgramiv(glprogram->id, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &maxsize);
+    name = (char*)malloc(maxsize);
+    for (int i=0; i<n; i++) {
+        DBG(e2=gles_glGetError();)
+        DBG(if(e2==GL_NO_ERROR))
+        {
+            gles_glGetActiveAttrib(glprogram->id, i, maxsize, NULL, &size, &type, name);
+            GLint id = gles_glGetAttribLocation(glprogram->id, name);
+            if(id!=-1) {
+                attribloc_t *glattribloc = NULL;
+                k = kh_put(attribloclist, glprogram->attribloc, id, &ret);
+                if(ret==0) {
+                    // already there
+                    glattribloc = kh_value(glprogram->attribloc, k);
+                    if(glattribloc->name)
+                        free(glattribloc->name);
+                } else {
+                    glattribloc = kh_value(glprogram->attribloc, k) = malloc(sizeof(attribloc_t));
+                }
+                memset(glattribloc, 0, sizeof(attribloc_t));
+                glattribloc->name = strdup(name);
+                glattribloc->size = size;
+                glattribloc->type = type;
+                glattribloc->index = id;
+                glattribloc->real_index = i;
+                int builtin = builtin_CheckVertexAttrib(glprogram, name, id);
+                glprogram->va_size[id] = n_uniform(type); // same as uniform
+                DBG(printf(" attrib #%d : \"%s\"%s type=%s size=%d\n", id, glattribloc->name, builtin?" (builtin) ":"", PrintEnum(glattribloc->type), glattribloc->size);)
+            }
+        }
+        DBG(else printf("LIBGL: Warning, getting Attrib #%d info failed with %s\n", i, PrintEnum(e2));)
+    }
+    free(name);
+}
+
+void gl4es_glLinkProgram(GLuint program) {
+    DBG(printf("glLinkProgram(%d)\n", program);)
+    FLUSH_BEGINEND;
+    CHECK_PROGRAM(void, program)
+    noerrorShim();
+
+    clear_program(glprogram);
 
     // check if attached shaders are compatible in term of varying...
     shaderconv_need_t needs;
@@ -556,131 +687,13 @@ void gl4es_glLinkProgram(GLuint program) {
     if(gles_glLinkProgram) {
         LOAD_GLES(glGetError);
         LOAD_GLES2(glGetProgramiv);
-        LOAD_GLES2(glGetActiveUniform);
-        LOAD_GLES2(glGetUniformLocation);
-        LOAD_GLES2(glGetActiveAttrib);
-        LOAD_GLES2(glGetAttribLocation);
         gles_glLinkProgram(glprogram->id);
         GLenum err = gles_glGetError();
-        int n=0;
-        int maxsize=0;
-        khint_t k;
-        int ret;
-        DBG(GLenum e2;)
         // Get Link Status
         gles_glGetProgramiv(glprogram->id, GL_LINK_STATUS, &glprogram->linked);
         DBG(printf(" link status = %d\n", glprogram->linked);)
         if(glprogram->linked) {
-            // init bluitin emulation first
-            builtin_Init(glprogram);
-            // Grab all Uniform
-            gles_glGetProgramiv(glprogram->id, GL_ACTIVE_UNIFORMS, &n);
-            gles_glGetProgramiv(glprogram->id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &maxsize);
-            khash_t(uniformlist) *uniforms = glprogram->uniform;
-            uniform_t *gluniform = NULL;
-            int uniform_cache = 0;
-            GLint size = 0;
-            GLenum type = 0;
-            GLchar *name = (char*)malloc(maxsize);
-            int tu_idx = 0;
-            for (int i=0; i<n; i++) {
-                gles_glGetActiveUniform(glprogram->id, i, maxsize, NULL, &size, &type, name);
-                DBG(e2=gles_glGetError();)
-                DBG(if(e2==GL_NO_ERROR))
-                {
-                    // remove any ending "[]" that could be present
-                    if(name[strlen(name)-1]==']' && strrchr(name, '[')) (*strrchr(name, '['))='\0';
-                    GLint id = gles_glGetUniformLocation(glprogram->id, name);
-                    if(id!=-1) {
-                        for (int j = 0; j<size; j++) {
-                            k = kh_put(uniformlist, uniforms, id, &ret);
-                            gluniform = kh_value(uniforms, k) = malloc(sizeof(uniform_t));
-                            memset(gluniform, 0, sizeof(uniform_t));
-                            if(j) {
-                                gluniform->name = malloc(strlen(name)+1+5);
-                                sprintf(gluniform->name, "%s[%d]", name, j);
-                            } else
-                                gluniform->name = strdup(name);
-                            gluniform->id = id;
-                            gluniform->internal_id = i;
-                            gluniform->size = size-j;
-                            gluniform->type = type;
-                            gluniform->cache_offs = uniform_cache+j*uniformsize(type);
-                            gluniform->cache_size = uniformsize(type)*(size-j);
-                            gluniform->builtin = builtin_CheckUniform(glprogram, name, id, size-j);
-                            // TextureUnit grabbing...
-                            if(type==GL_SAMPLER_CUBE) {
-                                glprogram->texunits[tu_idx].id = id;
-                                glprogram->texunits[tu_idx].type=TU_CUBE;
-                                glprogram->texunits[tu_idx].req_tu = glprogram->texunits[tu_idx].act_tu = 0;
-                                ++tu_idx;
-                            } else if (type==GL_SAMPLER_2D) {
-                                glprogram->texunits[tu_idx].id = id;
-                                glprogram->texunits[tu_idx].type=TU_TEX2D;
-                                glprogram->texunits[tu_idx].req_tu = glprogram->texunits[tu_idx].act_tu = 0;
-                                ++tu_idx;
-                            }
-                            DBG(printf(" uniform #%d : \"%s\"%s type=%s size=%d\n", id, gluniform->name, gluniform->builtin?" (builtin) ":"", PrintEnum(gluniform->type), gluniform->size);)
-                            if(gluniform->size==1) ++glprogram->num_uniform;
-                            id++;
-                        }
-                        uniform_cache += uniformsize(type)*size;
-                    }
-                }
-                DBG(else printf("LIBGL: Warning, getting Uniform #%d info failed with %s\n", i, PrintEnum(e2));)
-            }
-            free(name);
-            // reset uniform cache
-            if(glprogram->cache.cap < uniform_cache) {
-                glprogram->cache.cap=uniform_cache;
-                glprogram->cache.cache = malloc(glprogram->cache.cap);
-            }
-            memset(glprogram->cache.cache, 0, glprogram->cache.cap);
-            //Maybe Sampler uniform should not be initialized to 0, but to -1, to be sure the value is initialized?
-            if(glprogram->uniform) {
-                uniform_t *m;
-                khint_t k;
-                kh_foreach(glprogram->uniform, k, m,
-                    if(m->type == GL_SAMPLER_2D || m->type == GL_SAMPLER_CUBE)
-                        memset(glprogram->cache.cache+m->cache_offs, 0xff, m->cache_size);
-                )
-            }
-        
-            // Grab all Attrib
-            gles_glGetProgramiv(glprogram->id, GL_ACTIVE_ATTRIBUTES, &n);
-            gles_glGetProgramiv(glprogram->id, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &maxsize);
-            name = (char*)malloc(maxsize);
-            for (int i=0; i<n; i++) {
-                DBG(e2=gles_glGetError();)
-                DBG(if(e2==GL_NO_ERROR))
-                {
-                    gles_glGetActiveAttrib(glprogram->id, i, maxsize, NULL, &size, &type, name);
-                    GLint id = gles_glGetAttribLocation(glprogram->id, name);
-                    if(id!=-1) {
-                        attribloc_t *glattribloc = NULL;
-                        k = kh_put(attribloclist, glprogram->attribloc, id, &ret);
-                        if(ret==0) {
-                            // already there
-                            glattribloc = kh_value(glprogram->attribloc, k);
-                            if(glattribloc->name)
-                                free(glattribloc->name);
-                        } else {
-                            glattribloc = kh_value(glprogram->attribloc, k) = malloc(sizeof(attribloc_t));
-                        }
-                        memset(glattribloc, 0, sizeof(attribloc_t));
-                        glattribloc->name = strdup(name);
-                        glattribloc->size = size;
-                        glattribloc->type = type;
-                        glattribloc->index = id;
-                        glattribloc->real_index = i;
-                        int builtin = builtin_CheckVertexAttrib(glprogram, name, id);
-                        glprogram->va_size[id] = n_uniform(type); // same as uniform
-                        DBG(printf(" attrib #%d : \"%s\"%s type=%s size=%d\n", id, glattribloc->name, builtin?" (builtin) ":"", PrintEnum(glattribloc->type), glattribloc->size);)
-                    }
-                }
-                DBG(else printf("LIBGL: Warning, getting Attrib #%d info failed with %s\n", i, PrintEnum(e2));)
-            }
-            free(name);
+            fill_program(glprogram);
         } else {
             // should DBG the linker error?
             DBG(printf(" Link failled!\n");)
