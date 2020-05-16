7c2332d39238e9c6c88e7d348e73da9942612ef7
ptitSeb <sebastien.chev@gmail.com>
2020-05-16 16:40:11 +0200

    Fix a residual issue with S3TC texture handling

--- src/gl/texture_compressed.c
+++ src/gl/texture_compressed.c
@@ -207,17 +207,13 @@ void gl4es_glCompressedTexImage2D(GLenum target, GLint level, GLenum internalfor
                 type = GL_UNSIGNED_BYTE;
             } else {
                 format = (internalformat==GL_COMPRESSED_RGB_S3TC_DXT1_EXT || internalformat==GL_COMPRESSED_SRGB_S3TC_DXT1_EXT)?GL_RGB:GL_RGBA;
-                #ifdef PANDORA
                 type = (internalformat==GL_COMPRESSED_RGB_S3TC_DXT1_EXT || internalformat==GL_COMPRESSED_SRGB_S3TC_DXT1_EXT)?GL_UNSIGNED_SHORT_5_6_5:((internalformat==GL_COMPRESSED_RGBA_S3TC_DXT1_EXT)?GL_UNSIGNED_SHORT_5_5_5_1:GL_UNSIGNED_SHORT_4_4_4_4);
-                #else
-                type = (internalformat==GL_COMPRESSED_RGB_S3TC_DXT1_EXT || internalformat==GL_COMPRESSED_SRGB_S3TC_DXT1_EXT)?GL_UNSIGNED_SHORT_5_6_5:GL_UNSIGNED_SHORT_4_4_4_4;
-                #endif
+                if(level && bound->valid) {
+                    // don't mix type/format along mipmap...
+                    format = bound->format;
+                    type = bound->type;
+                }
             }
-        }
-        if(level && bound->valid) {
-            // don't mix type/format along mipmap...
-            format = bound->format;
-            type = bound->type;
         }
         int srgb = isDXTcSRGB(internalformat);
         int simpleAlpha = 0;
