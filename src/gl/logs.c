c26bd44b5c2c6ddfee92b575bb0fc5148426512c
ptitSeb <sebastien.chev@gmail.com>
2020-06-12 18:53:05 +0200

    {ANDROID] Get LIBGL logs in logcat

--- src/gl/logs.c
+++ src/gl/logs.c
@@ -8,7 +8,11 @@ void LogPrintf_NoPrefix(const char *fmt,...)
 {
 	va_list args;
 	va_start(args,fmt);
+	#ifdef ANDROID
+	__android_log_vprint(ANDROID_LOG_INFO, "LIBGL", fmt, args);
+	#else
 	vprintf(fmt,args);
+	#endif
 	va_end(args);
 }
 //----------------------------------------------------------------------------
@@ -17,16 +21,26 @@ void LogFPrintf(FILE *fp,const char *fmt,...)
 	fprintf(fp,log_prefix);
 	va_list args;
 	va_start(args,fmt);
+	#ifdef ANDROID
+	// also on logcat
+	__android_log_vprint(ANDROID_LOG_INFO, "LIBGL", fmt, args);
+	#else
 	vfprintf(fp,fmt,args);
 	va_end(args);
 }
 //----------------------------------------------------------------------------
 void LogPrintf(const char *fmt,...)
 {
+	#ifndef ANDROID
 	printf(log_prefix);
+	#endif
 	va_list args;
 	va_start(args,fmt);
+	#ifdef ANDROID
+	__android_log_vprint(ANDROID_LOG_INFO, "LIBGL", fmt, args);
+	#else
 	vprintf(fmt,args);
+	#endif
 	va_end(args);
 }
 //----------------------------------------------------------------------------
