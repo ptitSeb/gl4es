ce05473eadc956ab9a7367a2a6092584a652d7de
ptitSeb <sebastien.chev@gmail.com>
2020-07-16 18:16:02 +0200

    Added a few more debug enum

--- src/gl/debug.c
+++ src/gl/debug.c
@@ -118,8 +118,13 @@ const char* PrintEnum(GLenum what) {
         p(GL_MAX_TEXTURE_COORDS);
         // texture pack/unpack
         p(GL_UNPACK_ALIGNMENT);
-        p(GL_PACK_ALIGNMENT);
         p(GL_UNPACK_ROW_LENGTH);
+        p(GL_UNPACK_SKIP_PIXELS);
+        p(GL_UNPACK_SKIP_ROWS);
+        p(GL_PACK_ALIGNMENT);
+        p(GL_PACK_ROW_LENGTH);
+        p(GL_PACK_SKIP_PIXELS);
+        p(GL_PACK_SKIP_ROWS);
         // framebuffer
         p(GL_COLOR_ATTACHMENT0);
         p(GL_COLOR_ATTACHMENT1);
