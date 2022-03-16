54e9014d3b6687b85de6ce67f40e8c4e166faa4f
ptitSeb <sebastien.chev@gmail.com>
2022-03-16 15:50:23 +0100

    [GLES2] Fixed internal transpose functions (for #346)

--- src/gl/shaderconv.c
+++ src/gl/shaderconv.c
@@ -291,19 +291,19 @@ static const char* GLESHeader[] = {
 
 static const char* gl4es_transpose =
 "mat2 gl4es_transpose(mat2 m) {\n"
-" return mat2(m[0][0], m[0][1],\n"
-"             m[1][0], m[1][1]);\n"
+" return mat2(m[0][0], m[1][0],\n"
+"             m[0][1], m[1][1]);\n"
 "}\n"
 "mat3 gl4es_transpose(mat3 m) {\n"
-" return mat3(m[0][0], m[0][1], m[0][2],\n"
-"             m[1][0], m[1][1], m[1][2],\n"
-"             m[2][0], m[2][1], m[2][2]);\n"
+" return mat3(m[0][0], m[1][0], m[2][0],\n"
+"             m[0][1], m[1][1], m[2][1],\n"
+"             m[0][2], m[1][2], m[2][2]);\n"
 "}\n"
 "mat4 gl4es_transpose(mat4 m) {\n"
-" return mat4(m[0][0], m[0][1], m[0][2], m[0][3],\n"
-"             m[1][0], m[1][1], m[1][2], m[1][3],\n"
-"             m[2][0], m[2][1], m[2][2], m[2][3],\n"
-"             m[3][0], m[3][1], m[3][2], m[3][3]);\n"
+" return mat4(m[0][0], m[1][0], m[2][0], m[3][0],\n"
+"             m[0][1], m[1][1], m[2][1], m[3][1],\n"
+"             m[0][2], m[1][2], m[2][2], m[3][2],\n"
+"             m[0][3], m[1][3], m[2][3], m[3][3]);\n"
 "}\n";
 
 static const char* HackAltPow = 
