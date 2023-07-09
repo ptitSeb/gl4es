84df8f69570518cc62db190e1e932fc457d08ca3
Mathias-Boulay <mathiasboulay@free.fr>
2023-07-09 22:50:03 +0200

    Fix[shaderconv]: Small memory leak

--- src/gl/shaderconv.c
+++ src/gl/shaderconv.c
@@ -1257,6 +1257,8 @@ char* ConvertShader(const char* pEntry, int isVertex, shaderconv_need_t *need)
     printf("New Shader source:\n%s\n", Tmp);
   }
   // clean preproc'd source
+  if(versionString != NULL)
+    free(versionString);
   if(pEntry!=pBuffer)
     free(pBuffer);
   return Tmp;
