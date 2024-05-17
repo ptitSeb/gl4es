61e20c52c0d0d899854ada26c7b9e458b01a3a23
Hubert Maier <raziel-@users.noreply.github.com>
2024-05-17 23:03:48 +0200

    JANITORIAL: Fix beeing and fist typos in planes.c

--- src/gl/planes.c
+++ src/gl/planes.c
@@ -14,7 +14,7 @@ void APIENTRY_GL4ES gl4es_glClipPlanef(GLenum plane, const GLfloat *equation)
         If the dot product of the eye coordinates of a vertex with the stored plane equation components is positive or zero, 
         the vertex is in with respect to that clipping plane. Otherwise, it is out.
     */
-    PUSH_IF_COMPILING(glClipPlanef);    //TODO: fix that, equation is an array and should be copied before beeing put in the stack
+    PUSH_IF_COMPILING(glClipPlanef);    //TODO: fix that, equation is an array and should be copied before being put in the stack
     if((plane<GL_CLIP_PLANE0) || (plane>=GL_CLIP_PLANE0+hardext.maxplanes)) {
         errorShim(GL_INVALID_ENUM);
         return;
@@ -23,7 +23,7 @@ void APIENTRY_GL4ES gl4es_glClipPlanef(GLenum plane, const GLfloat *equation)
         LOAD_GLES2(glClipPlanef);
         errorGL();
         gles_glClipPlanef(plane, equation);
-    } else {    // TODO: should fist compute the clipplane and compare to stored one before sending to hardware
+    } else {    // TODO: should first compute the clipplane and compare to stored one before sending to hardware
         int p = plane-GL_CLIP_PLANE0;
         matrix_vector(getInvMVMat(), equation, glstate->planes[p]); //Tested, seems ok
         noerrorShim();
