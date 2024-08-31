8ad6fbf117b4a3251ae3c473114d6e5b60f7d48e
Duron27 <duron27@gmail.com>
2024-08-31 09:43:59 -0400

    Update texture.c

--- src/gl/texture.c
+++ src/gl/texture.c
@@ -168,6 +168,16 @@ void internal2format_type(GLenum internalformat, GLenum *format, GLenum *type)
             *format = GL_DEPTH_STENCIL;
             *type = GL_UNSIGNED_INT_24_8;
             break;
+        case GL_R16F:
+            if(!hardext.rgtex)
+                *format = GL_RGB;
+        else
+            *format = GL_RED;
+        if(!hardext.halffloattex)
+            *type = GL_UNSIGNED_BYTE;
+        else
+            *type = GL_HALF_FLOAT_OES;
+            break;
         case GL_RGBA16F:
             *format = GL_RGBA;
             *type = (hardext.halffloattex)?GL_HALF_FLOAT_OES:GL_UNSIGNED_BYTE;
@@ -897,6 +907,15 @@ void APIENTRY_GL4ES gl4es_glTexImage2D(GLenum target, GLint level, GLint interna
                   GLsizei width, GLsizei height, GLint border,
                   GLenum format, GLenum type, const GLvoid *data) {
     DBG(printf("glTexImage2D on target=%s with unpack_row_length(%i), size(%i,%i) and skip(%i,%i), format(internal)=%s(%s), type=%s, data=%p, level=%i (mipmap_need=%i, mipmap_auto=%i, base_level=%i, max_level=%i) => texture=%u (streamed=%i), glstate->list.compiling=%d\n", PrintEnum(target), glstate->texture.unpack_row_length, width, height, glstate->texture.unpack_skip_pixels, glstate->texture.unpack_skip_rows, PrintEnum(format), (internalformat==3)?"3":(internalformat==4?"4":PrintEnum(internalformat)), PrintEnum(type), data, level, glstate->texture.bound[glstate->texture.active][what_target(target)]->mipmap_need, glstate->texture.bound[glstate->texture.active][what_target(target)]->mipmap_auto, glstate->texture.bound[glstate->texture.active][what_target(target)]->base_level, glstate->texture.bound[glstate->texture.active][what_target(target)]->max_level, glstate->texture.bound[glstate->texture.active][what_target(target)]->texture, glstate->texture.bound[glstate->texture.active][what_target(target)]->streamed, glstate->list.compiling);)
+    
+    if(data==NULL && (internalformat == GL_RGB16F || internalformat == GL_RGBA16F))
+    internal2format_type(internalformat, &format, &type);
+
+    if(internalformat == GL_R16F ) internal2format_type(internalformat, &format, &type);
+
+    if(data==NULL && (internalformat == GL_RED || internalformat == GL_RGB))
+        internal2format_type(internalformat, &format, &type);
+    
     // proxy case
     const GLuint itarget = what_target(target);
     const GLuint rtarget = map_tex_target(target);
